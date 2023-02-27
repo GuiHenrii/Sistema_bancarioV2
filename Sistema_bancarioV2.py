c6edb37c14fb068a370f9b7b54b217b5490fd354
Guilherme <guilhermehenriquekf@gmail.com>
2023-02-26 23:47:44 -0300

    Sistema_bancario

---
+++ Sistema_bancarioV2.py
@@ -0,0 +1,21 @@
+conta_normal = True
+conta_universitaria = False
+
+saldo = 2000
+saque = 1500
+cheque_especial = 450
+
+if conta_normal:
+    if saldo >= saque:
+        print('Saque realizado com sucesso!')
+    elif saque <= (saldo + cheque_especial):
+        print('Saque realizado com uso do cheque especial!')
+    else:
+        print('Não foi possivel realizar o saque, Saldo insuficiente!')
+elif conta_universitaria:
+    if saldo >= saque:
+        print('Saque realizado com sucesso!')
+    else:
+        print('Saldo insuficiente!')
+else:
+    print('O sistema não reconheceu a sua conta, entre em contato com o seu gerente!')
